2b87bb2138a1c42d5137606b5f4d533b8d5692f0
unknown <ktaher642@gmail.com>
2021-09-22 19:02:12 -1200

    Order Screen add in UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/screens/cart_screen.dart';
+import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 
@@ -36,6 +37,7 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routesName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
         },
       ),
     );
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/orders.dart';
+import 'package:shop_app/providers/orders.dart' show Orders;
+import 'package:shop_app/widgets/app_drawer.dart';
+import 'package:shop_app/widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
   @override
   Widget build(BuildContext context) {
-    final orders = Provider.of<Orders>(context);
+    final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders Details'),
       ),
-      body: ListView(),
+      drawer: AppDrawer(),
+      body: ListView.builder(
+        itemCount: orderData.orders.length,
+        itemBuilder: (ctx, i) => OrderItem(
+          orderData.orders[i],
+        ),
+      ),
     );
   }
 }
